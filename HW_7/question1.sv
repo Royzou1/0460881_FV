5da6f1439d0b4c9bf3028485c7a6b7902aef22d4
Kfir-Barzilay <kfir1602@gmail.com>
2025-01-21 20:00:57 +0200

    a

--- HW_7/question1.sv
+++ HW_7/question1.sv
@@ -1,13 +1,8 @@
-
-
 module hw7 (
     input clk,
     input rst,
     input en,
     input ok
 );
-
 assume property (@(posedge clk) en |=> s_eventually(ok));
 assert property (@(posedge clk) en |=> (1'b1 s_until ok));
-
-
