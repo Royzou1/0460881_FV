c1957a059550d9a413080280c2008b610d1c0826
Kfir-Barzilay <kfir1602@gmail.com>
2025-01-23 12:37:33 +0200

    aaa

---
+++ HW_7/q2.tcl
@@ -0,0 +1,13 @@
+check_sec -compile_context spec
+analyze -sv09 spec.v
+elaborate -bbox_mul 32
+
+check_sec -compile_context spec
+analyze -sv09 imp.v
+elaborate -bbox_mul 32
+
+reset ~rst
+clock clk
+
+check_sec -setup
+check_sec -interface
